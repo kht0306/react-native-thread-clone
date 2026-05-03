0b1506d16bea278b9584e5a32ec8f5f00d176fb8
HungtaeKim <kht0306@gmail.com>
2026-05-03 13:05:04 +0900

    feat: 로그인 전 모달 기능 추가 및 tab에 backBehavior="history" 옵션 적용

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,8 +1,9 @@
-import { useRouter } from "expo-router";
+import { usePathname, useRouter } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+  const pathname = usePathname();
   return (
     <View
       style={{
@@ -13,12 +14,16 @@ export default function Index() {
     >
       <View>
         <TouchableOpacity onPress={() => router.push("/")}>
-          <Text>For you</Text>
+          <Text style={{ color: pathname === "/" ? "red" : "black" }}>
+            For you
+          </Text>
         </TouchableOpacity>
       </View>
       <View>
         <TouchableOpacity onPress={() => router.push("/following")}>
-          <Text>Following</Text>
+          <Text style={{ color: pathname === "/" ? "balck" : "red" }}>
+            Following
+          </Text>
         </TouchableOpacity>
       </View>
       <View>
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,88 +1,145 @@
+import { useState } from "react";
+import { Modal, Text, TouchableOpacity, View } from "react-native";
+
 import { Ionicons } from "@expo/vector-icons";
 import { Tabs, useRouter } from "expo-router";
 
 export default function TabLayout() {
   const router = useRouter();
+  const isLoggedIn = true;
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
+
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+  };
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Tabs.Screen
-        name="(home)"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="home"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
+    <>
+      <Tabs
+        backBehavior="history"
+        screenOptions={{
+          headerShown: false,
         }}
-      />
-      <Tabs.Screen
-        name="search"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="search"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="add"
-        listeners={{
-          tabPress: (e) => {
-            e.preventDefault();
-            router.navigate("/modal");
-          },
-        }}
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons name="add" size={24} color={focused ? "black" : "gray"} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="activity"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="heart"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="[username]"
-        options={{
-          tabBarLabel: () => null,
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name="person"
-              size={24}
-              color={focused ? "black" : "gray"}
-            />
-          ),
-        }}
-      />
-      {/* post의 상세페이지는 탭바의 간섭을 받지 않고 별도의 모달로 띄우기 위해 href: null로 설정 */}
-      <Tabs.Screen
-        name="(post)/[username]/post/[postID]"
-        options={{
-          href: null,
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="(home)"
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="home"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="search"
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="search"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="add"
+          listeners={{
+            tabPress: (e) => {
+              e.preventDefault();
+              if (isLoggedIn) {
+                router.navigate("/modal");
+              } else {
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="add"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="activity"
+          listeners={{
+            tabPress: (e) => {
+              if (!isLoggedIn) {
+                e.preventDefault();
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="heart"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="[username]"
+          listeners={{
+            tabPress: (e) => {
+              if (!isLoggedIn) {
+                e.preventDefault();
+                openLoginModal();
+              }
+            },
+          }}
+          options={{
+            tabBarLabel: () => null,
+            tabBarIcon: ({ focused }) => (
+              <Ionicons
+                name="person"
+                size={24}
+                color={focused ? "black" : "gray"}
+              />
+            ),
+          }}
+        />
+        {/* post의 상세페이지는 탭바의 간섭을 받지 않고 별도의 모달로 띄우기 위해 href: null로 설정 */}
+        <Tabs.Screen
+          name="(post)/[username]/post/[postID]"
+          options={{
+            href: null,
+          }}
+        />
+      </Tabs>
+      <Modal visible={isLoginModalOpen} transparent animationType="slide">
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <View style={{ backgroundColor: "white", padding: 20 }}>
+            <Text>Login Modal</Text>
+            <TouchableOpacity onPress={closeLoginModal}>
+              <Ionicons name="close" size={24} color="black" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    </>
   );
 }
