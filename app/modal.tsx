fbcc20afe6e1bee5b0aa011f1ed654cc12f64575
HungtaeKim <kht0306@gmail.com>
2026-05-06 22:57:33 +0900

    study: 260506 클론 코딩 내역 커밋
    기기의 위치 관련 기능 추가

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,10 +1,12 @@
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
-// import * as Location from "expo-location";
+import * as Location from "expo-location";
 import { useRouter } from "expo-router";
 import React, { useState } from "react";
 import {
+  Alert,
   FlatList,
   Image,
+  Linking,
   Pressable,
   StyleSheet,
   Text,
@@ -95,37 +97,42 @@ export default function Modal() {
   const removeImageFromThread = (id: string, uriToRemove: string) => {};
 
   const getMyLocation = async (id: string) => {
-    // let { status } = await Location.requestForegroundPermissionsAsync();
-    // console.log("getMyLocation", status);
-    // if (status !== "granted") {
-    //   Alert.alert(
-    //     "Location permission not granted",
-    //     "Please grant location permission to use this feature",
-    //     [
-    //       {
-    //         text: "Open settings",
-    //         onPress: () => {
-    //           Linking.openSettings();
-    //         },
-    //       },
-    //       {
-    //         text: "Cancel",
-    //       },
-    //     ],
-    //   );
-    //   return;
-    // }
-    // const location = await Location.getCurrentPositionAsync({});
-    // setThreads((prevThreads) =>
-    //   prevThreads.map((thread) =>
-    //     thread.id === id
-    //       ? {
-    //           ...thread,
-    //           location: [location.coords.latitude, location.coords.longitude],
-    //         }
-    //       : thread,
-    //   ),
-    // );
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    console.log("getMyLocation", status);
+    if (status !== "granted") {
+      Alert.alert(
+        "Location permission required",
+        "Please grant location permission to use this feature",
+        [
+          {
+            text: "Open settings",
+            onPress: () => Linking.openSettings(),
+          },
+          {
+            text: "Cancel",
+          },
+        ],
+      );
+      // setErrorMsg("Please grant location permission to use this feature");
+      return;
+    }
+
+    const location = await Location.getCurrentPositionAsync({});
+    const address = await Location.reverseGeocodeAsync({
+      latitude: 37.53,
+      longitude: 127.02,
+    });
+    console.log(address);
+    setThreads((prevThreads) =>
+      prevThreads.map((thread) =>
+        thread.id === id
+          ? {
+              ...thread,
+              location: [location.coords.latitude, location.coords.longitude],
+            }
+          : thread,
+      ),
+    );
   };
 
   const renderThreadItem = ({
